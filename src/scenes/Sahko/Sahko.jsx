9e673b89b4a12e5c4c8263c4ee02b921e7a88a98
Vesku <pellikka.vesku@gmail.com>
2025-02-08 14:59:06 +0200

    Lisätty Sahko-komponentti näkymään sovelluksessa

---
+++ src/scenes/Sahko/Sahko.jsx
@@ -0,0 +1,14 @@
+import { orientations } from "../../utils/types"   // screen orientation type
+import './Sahko.css'
+import React from "react";
+
+
+function Sahko() {
+  return (
+    <div className="sahko-container">
+      <p>to doo sähkön hinta</p>
+    </div>
+  );
+}
+
+export default Sahko;
---
+++ src/scenes/Sahko/index.js
@@ -0,0 +1,1 @@
+export { default } from './Sahko.jsx'
