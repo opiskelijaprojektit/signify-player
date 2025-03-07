c04d76975622c0cf937320ac6f5120f275724c82
Ada Vuorinen <ada.vuorinen@gmail.com>
2025-03-07 21:53:56 +0200

    lisää CSS-tyylimäärittelyt

--- src/scenes/quote/Quote.jsx
+++ src/scenes/quote/Quote.jsx
@@ -8,12 +8,22 @@ import backgroundImage from './bg_quote.jpg'
  * @author Ada Vuorinen
  */
 function Quote(props) {
-  
+
   return (
     <div className="quote" style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div>
-            TO DO: show quote
-        </div>
+      <div className="quote_header">
+        <h1>Quote of the Day</h1>
+      </div>
+      <div className="quote_text">
+        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit.&rdquo;</p>
+      </div>
+      <div className="quote_author">
+        <p>&mdash; Maecenas Sapien</p>
+      </div>
+      <div className="quote_attribution">
+        <p>Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
+        </p>
+      </div>
     </div>
   )  
 }
