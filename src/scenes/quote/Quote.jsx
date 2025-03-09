a32c404836d25cf1c91ac4910bf41d9923feb4a9
Ada Vuorinen <ada.vuorinen@gmail.com>
2025-03-09 15:20:15 +0200

    lisää quotes-listan ja valitsee päivän lainauksen

--- src/scenes/quote/Quote.jsx
+++ src/scenes/quote/Quote.jsx
@@ -1,24 +1,33 @@
 import './Quote.css'
 import backgroundImage from './bg_quote.jpg'
+import Quotes from './quotes.json'
 
 /**
  * A component that displays an inspirational quote that changes daily.
- *
+ * Quotes are pulled from a list of 367 quotes.
+ * 
  * @component
  * @author Ada Vuorinen
  */
 function Quote(props) {
 
+  // Calculate the day of the year as integer between 1-366
+  // First, get today's date and time
+  let date = new Date()
+  // Convert to UTC to avoid DST problems and subtract start of year
+  let dayOfYear = Math.floor((Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000)
+  
+  // Return the Quote component
   return (
     <div className="quote" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="quote_header">
         <h1>Quote of the Day</h1>
       </div>
       <div className="quote_text">
-        <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit.&rdquo;</p>
+        <p>&ldquo;{ Quotes && Quotes[dayOfYear-1].q }&rdquo;</p>
       </div>
       <div className="quote_author">
-        <p>&mdash; Maecenas Sapien</p>
+        <p>&mdash; { Quotes && Quotes[dayOfYear-1].a }</p>
       </div>
       <div className="quote_attribution">
         <p>Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
