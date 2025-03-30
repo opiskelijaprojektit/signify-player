92292bca3845dc4a52e94e949576ed905c5a87d8
Koodaajaguru <jebeninja@gmail.com>
2025-03-30 20:54:46 +0300

    Css muutoksia ja koodin yksinkertaistamista

--- src/scenes/calendar/Calendar.jsx
+++ src/scenes/calendar/Calendar.jsx
@@ -81,64 +81,9 @@ const Calendar = () => {
           ))}
         </div>
       </div>
-      <div className="events">
-        {showEventPopup && (
-          <div className="event-popup">
-            <div className="time-input">
-              <div className="event-popup-time">Time</div>
-              <input
-                type="number"
-                name="hours"
-                min={0}
-                max={24}
-                className="hours"
-                value={eventTime.hours}
-                onChange={handleTimeChange}
-              />
-              <input
-                type="number"
-                name="minutes"
-                min={0}
-                max={60}
-                className="minutes"
-                value={eventTime.minutes}
-                onChange={handleTimeChange}
-              />
-            </div>
-            <textarea
-              placeholder="Enter Event Text (Maximum 60 Characters)"
-              value={eventText}
-              onChange={(e) => {
-                if (e.target.value.length <= 60) {
-                  setEventText(e.target.value)
-                }
-              }}
-            ></textarea>
-            <button className="event-popup-btn" onClick={handleEventSubmit}>
-              {editingEvent ? 'Update Event' : 'Add Event'}
-            </button>
-            <button className="close-event-popup" onClick={() => setShowEventPopup(false)}>
-              <i className="bx bx-x"></i>
-            </button>
-          </div>
-        )}
-        {events.map((event, index) => (
-          <div className="event" key={index}>
-            <div className="event-date-wrapper">
-              <div className="event-date">{`${
-                monthsOfYear[event.date.getMonth()]
-              } ${event.date.getDate()}, ${event.date.getFullYear()}`}</div>
-              <div className="event-time">{event.time}</div>
-            </div>
-            <div className="event-text">{event.text}</div>
-            <div className="event-buttons">
-              <i className="bx bxs-edit-alt" onClick={() => handleEditEvent(event)}></i>
-              <i className="bx bxs-message-alt-x" onClick={() => handleDeleteEvent(event.id)}></i>
-            </div>
-          </div>
-        ))}
+
       </div>
-    </div>
+   
   )
 }
 
